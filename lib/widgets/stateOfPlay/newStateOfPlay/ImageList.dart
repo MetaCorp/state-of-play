6f7da5467a95a38f5bcb86b09773b5cb6d48301c
PortableGuigui <ma>
2021-01-12 18:24:39 +0100

    Fix MyImagePicker maximages

--- lib/widgets/stateOfPlay/newStateOfPlay/ImageList.dart
+++ lib/widgets/stateOfPlay/newStateOfPlay/ImageList.dart
@@ -1,16 +1,15 @@
-
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_tests/widgets/stateOfPlay/newStateOfPlay/EditImage.dart';
 import 'package:flutter_tests/widgets/stateOfPlay/newStateOfPlay/newStateOfPlayDetails/NewStateOfPlayDetailsRoomDecoration.dart';
 
-
 typedef DeleteCallback = Function(dynamic);
 typedef UpdateCallback = Function(Uint8List, int);
 
 class ImageList extends StatefulWidget {
-  ImageList({ Key key, this.imagesType, this.onDelete,this.onUpdate }) : super(key: key);
+  ImageList({Key key, this.imagesType, this.onDelete, this.onUpdate})
+      : super(key: key);
 
   List<dynamic> imagesType;
   DeleteCallback onDelete;
@@ -21,93 +20,86 @@ class ImageList extends StatefulWidget {
 }
 
 class _ImageListState extends State<ImageList> {
-
-
-
-
   @override
   Widget build(BuildContext context) {
-    
     return Container(
       margin: EdgeInsets.only(top: 8),
-      child: Column(
-        children: [
-          Wrap(
-            runSpacing: 8,
-            spacing: 8,
-            alignment: WrapAlignment.start,
-            runAlignment: WrapAlignment.start,
-            children: widget.imagesType.map((imageType) {
-              Widget image;
+      child: Column(children: [
+        Wrap(
+          runSpacing: 8,
+          spacing: 8,
+          alignment: WrapAlignment.start,
+          runAlignment: WrapAlignment.start,
+          children: widget.imagesType.map((imageType) {
+            Widget image;
 
-              if (imageType.type == "file")
-                image = Image.file(
-                  imageType.image,
-                  width: 150,
-                  height: 150,
-                  fit: BoxFit.fill,
-                  // frameBuilder: (BuildContext context, Widget child, int frame,
-                  //   bool wasSynchronouslyLoaded) {
-                  //   if (wasSynchronouslyLoaded) {
-                  //     return child;
-                  //   } return Container(
-                  //     width: 150,
-                  //     height: 150,
-                  //     child: Center(
-                  //       child: CircularProgressIndicator(
-                  //         value: null,
-                  //       ),
-                  //     ),
-                  //   );
-                  //   }
+            if (imageType.type == "file")
+              image = Image.file(
+                imageType.image,
+                width: 150,
+                height: 150,
+                fit: BoxFit.fill,
+                // frameBuilder: (BuildContext context, Widget child, int frame,
+                //   bool wasSynchronouslyLoaded) {
+                //   if (wasSynchronouslyLoaded) {
+                //     return child;
+                //   } return Container(
+                //     width: 150,
+                //     height: 150,
+                //     child: Center(
+                //       child: CircularProgressIndicator(
+                //         value: null,
+                //       ),
+                //     ),
+                //   );
+                //   }
               );
-              
-              else if (imageType.type == "memory")
-                image = Image.memory(
-                  imageType.image,
-                  width: 150,
-                  height: 150,
-                  fit: BoxFit.fill,
-                  // frameBuilder: (BuildContext context, Widget child, int frame,
-                  //   bool wasSynchronouslyLoaded) {
-                  //   if (wasSynchronouslyLoaded) {
-                  //     return child;
-                  //   } return Container(
-                  //     width: 150,
-                  //     height: 150,
-                  //     child: Center(
-                  //       child: CircularProgressIndicator(
-                  //         value: null,
-                  //       ),
-                  //     ),
-                  //   );
-                  //   }
-                );
-              else
-                image = Image.network(
-                  imageType.image,
-                  width: 150,
-                  height: 150,
-                  fit: BoxFit.fill,
-                  loadingBuilder: (BuildContext context, Widget child,
+            else if (imageType.type == "memory")
+              image = Image.memory(
+                imageType.image,
+                width: 150,
+                height: 150,
+                fit: BoxFit.cover,
+                // frameBuilder: (BuildContext context, Widget child, int frame,
+                //   bool wasSynchronouslyLoaded) {
+                //   if (wasSynchronouslyLoaded) {
+                //     return child;
+                //   } return Container(
+                //     width: 150,
+                //     height: 150,
+                //     child: Center(
+                //       child: CircularProgressIndicator(
+                //         value: null,
+                //       ),
+                //     ),
+                //   );
+                //   }
+              );
+            else
+              image = Image.network(
+                imageType.image,
+                width: 150,
+                height: 150,
+                fit: BoxFit.cover,
+                loadingBuilder: (BuildContext context, Widget child,
                     ImageChunkEvent loadingProgress) {
-                    if (loadingProgress == null) return child;
-                    return Container(
-                      width: 150,
-                      height: 150,
-                      child: Center(
-                        child: CircularProgressIndicator(
-                          value: loadingProgress.expectedTotalBytes != null
-                              ? loadingProgress.cumulativeBytesLoaded /
-                                  loadingProgress.expectedTotalBytes
-                              : null,
-                        ),
+                  if (loadingProgress == null) return child;
+                  return Container(
+                    width: 150,
+                    height: 150,
+                    child: Center(
+                      child: CircularProgressIndicator(
+                        value: loadingProgress.expectedTotalBytes != null
+                            ? loadingProgress.cumulativeBytesLoaded /
+                                loadingProgress.expectedTotalBytes
+                            : null,
                       ),
-                    );
-                  },
-                );
+                    ),
+                  );
+                },
+              );
 
-              return GestureDetector(
+            return GestureDetector(
                 child: Container(
                   width: 150,
                   height: 150,
@@ -116,14 +108,14 @@ class _ImageListState extends State<ImageList> {
                       image,
                       Align(
                         alignment: Alignment.topRight,
-                          child: ButtonTheme(    
+                        child: ButtonTheme(
                           minWidth: 26.0,
                           height: 60,
-                          child: FlatButton(   
-                            padding: EdgeInsets.fromLTRB(0, 0, 7.5, 0),   
+                          child: FlatButton(
+                            padding: EdgeInsets.fromLTRB(0, 0, 7.5, 0),
                             shape: CircleBorder(),
                             child: Icon(Icons.close, size: 22),
-                            onPressed: () { 
+                            onPressed: () {
                               setState(() {
                                 showDeleteImage(imageType);
                               });
@@ -136,49 +128,45 @@ class _ImageListState extends State<ImageList> {
                 ),
                 onTap: () async {
                   FocusScope.of(context).unfocus();
-                  Uint8List editedImage = await Navigator.push(context, PageRouteBuilder(pageBuilder: (_, __, ___) => EditImage(
-                    image: imageType.image,
-                    type: imageType.type,
-                  )));
-                  widget.onUpdate(editedImage, widget.imagesType.indexOf(imageType));
+                  Uint8List editedImage = await Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                          pageBuilder: (_, __, ___) => EditImage(
+                                image: imageType.image,
+                                type: imageType.type,
+                              )));
+                  widget.onUpdate(
+                      editedImage, widget.imagesType.indexOf(imageType));
                   //setState(() { });
                   // setState(() {
                   //   widget.imagesType[widget.imagesType.indexOf(imageType)] = new ImageType(image: editedImage, type: 'memory');
                   // });
-                }
-              );
-            }).toList(),
-          )
-        ]
-      ),
+                });
+          }).toList(),
+        )
+      ]),
     );
   }
 
-  showDeleteImage(imageType) async  {
+  showDeleteImage(imageType) async {
     await showDialog(
-      context: context,
-      child: AlertDialog(
-        content: Text("Supprimer l'image ?"),
-        actions: [
-          new FlatButton(
-            child: Text('ANNULER'),
-            onPressed: () {
-              Navigator.pop(context);
-            }
-          ),
-          new FlatButton(
-            child: Text('SUPPRIMER'),
-            onPressed: () async { 
-              widget.onDelete({
-                "type": imageType.type,
-                "image": imageType.image
-              });
-              Navigator.pop(context);
-            }
-          )
-        ],
-      )
-    );
-  }  
-
+        context: context,
+        child: AlertDialog(
+          content: Text("Supprimer l'image ?"),
+          actions: [
+            new FlatButton(
+                child: Text('ANNULER'),
+                onPressed: () {
+                  Navigator.pop(context);
+                }),
+            new FlatButton(
+                child: Text('SUPPRIMER'),
+                onPressed: () async {
+                  widget.onDelete(
+                      {"type": imageType.type, "image": imageType.image});
+                  Navigator.pop(context);
+                })
+          ],
+        ));
+  }
 }
--- lib/widgets/stateOfPlay/newStateOfPlay/MyImagePicker.dart
+++ lib/widgets/stateOfPlay/newStateOfPlay/MyImagePicker.dart
@@ -7,12 +7,12 @@ import 'package:multi_image_picker/multi_image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:uuid/uuid.dart';
 
-
 typedef SelectCallback = void Function(File);
 
 class MyImagePicker extends StatefulWidget {
-
-  MyImagePicker({ Key key, this.onSelect, this.imagesCount, this.isMultiSelection }) : super(key: key);
+  MyImagePicker(
+      {Key key, this.onSelect, this.imagesCount, this.isMultiSelection})
+      : super(key: key);
 
   SelectCallback onSelect;
   int imagesCount;
@@ -26,43 +26,37 @@ class _MyImagePickerState extends State<MyImagePicker> {
   String _error = 'No Error Dectected';
   Uuid uuid = Uuid();
 
-
   _imgFromCamera() async {
     File image = await ImagePicker.pickImage(
-      preferredCameraDevice: CameraDevice.rear,
-      source: ImageSource.camera, imageQuality: 50
-    );
+        preferredCameraDevice: CameraDevice.rear,
+        source: ImageSource.camera,
+        imageQuality: 50);
 
     // Uint8List bytes = await image.readAsBytes();
-    if(image == null)
-      return;
+    if (image == null) return;
 
     widget.onSelect(image);
   }
 
   _imgFromGallery() async {
-    File image = await  ImagePicker.pickImage(
-      source: ImageSource.gallery,
-      imageQuality: 50
-    );
-    if(image == null)
-      return;
-      
+    File image = await ImagePicker.pickImage(
+        source: ImageSource.gallery, imageQuality: 50);
+    if (image == null) return;
+
     widget.onSelect(image);
   }
 
   Future<void> _imgsFromGallery() async {
     List<Asset> images = List<Asset>();
 
-
     try {
       images = await MultiImagePicker.pickImages(
-        maxImages: 5,
+        maxImages: 5 - widget.imagesCount,
         enableCamera: true,
         selectedAssets: images,
         cupertinoOptions: CupertinoOptions(takePhotoIcon: "chat"),
         materialOptions: MaterialOptions(
-          actionBarColor: "#abcdef",
+          actionBarColor: "#4AAAD3",
           actionBarTitle: "Housely",
           allViewTitle: "Toutes les photos",
           useDetailsView: false,
@@ -79,78 +73,74 @@ class _MyImagePickerState extends State<MyImagePicker> {
     // setState to update our non-existent appearance.
     if (!mounted) return;
 
-    images.forEach((image) async  { 
+    images.forEach((image) async {
       ByteData data = await image.getByteData();
       final buffer = data.buffer;
 
-      File file = await File(await getFilePath()).create(recursive: true).then((file) async => await file.writeAsBytes(
-        buffer.asUint8List(data.offsetInBytes, data.lengthInBytes)));
+      File file = await File(await getFilePath()).create(recursive: true).then(
+          (file) async => await file.writeAsBytes(
+              buffer.asUint8List(data.offsetInBytes, data.lengthInBytes)));
 
       widget.onSelect(file);
     });
   }
 
   Future<String> getFilePath() async {
-
-    Directory appDocumentsDirectory = await getApplicationDocumentsDirectory(); // 1
+    Directory appDocumentsDirectory =
+        await getApplicationDocumentsDirectory(); // 1
     String appDocumentsPath = appDocumentsDirectory.path;
-    String filePath = '$appDocumentsPath/NewStateOfPlay/MultiSelected/${uuid.v1()}.png';
+    String filePath =
+        '$appDocumentsPath/NewStateOfPlay/MultiSelected/${uuid.v1()}.png';
 
     return filePath;
   }
 
-
   void _showPicker(context) {
     showModalBottomSheet(
-      context: context,
-      builder: (BuildContext bc) {
-        return SafeArea(
-          child: Container(
-            child: Wrap(
-              children: [
-                ListTile(
-                  leading: Icon(Icons.photo_library),
-                  title: Text('Galerie'),
-                  onTap: () {
-                    widget.isMultiSelection ? _imgsFromGallery() : _imgFromGallery();
-                    Navigator.of(context).pop();
-                  }),
-                ListTile(
-                  leading: Icon(Icons.photo_camera),
-                  title: Text('Caméra'),
-                  onTap: () {
-                    _imgFromCamera();
-                    Navigator.of(context).pop();
-                  },
-                ),
-              ],
+        context: context,
+        builder: (BuildContext bc) {
+          return SafeArea(
+            child: Container(
+              child: Wrap(
+                children: [
+                  ListTile(
+                      leading: Icon(Icons.photo_library),
+                      title: Text('Galerie'),
+                      onTap: () {
+                        widget.isMultiSelection
+                            ? _imgsFromGallery()
+                            : _imgFromGallery();
+                        Navigator.of(context).pop();
+                      }),
+                  ListTile(
+                    leading: Icon(Icons.photo_camera),
+                    title: Text('Caméra'),
+                    onTap: () {
+                      _imgFromCamera();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
             ),
-          ),
-        );
-      }
-    );
+          );
+        });
   }
 
   @override
   Widget build(BuildContext context) {
-
-
     return Row(
       children: [
         Spacer(),
         Text(widget.imagesCount.toString() + '/5'),
-        SizedBox(
-          width: 16
-        ),
+        SizedBox(width: 16),
         RaisedButton(
           color: Colors.grey[200],
-          child: Row(
-            children: [
-              Icon(Icons.camera_alt),
-              SizedBox(width: 8),
-              Text('Ajouter une photo')
-            ]
-          ),
+          child: Row(children: [
+            Icon(Icons.camera_alt),
+            SizedBox(width: 8),
+            Text('Ajouter une photo')
+          ]),
           onPressed: widget.imagesCount < 5 ? () => _showPicker(context) : null,
         ),
       ],
